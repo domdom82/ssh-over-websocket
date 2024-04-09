9fa78d2601cffcd039c8de0b13cd1646d1db3f0e
Dominik Froehlich <dominik.froehlich@sap.com>
2024-04-09 14:19:40 +0200

    wrap ssh in websocket and run a command to test it

--- client/main.go
+++ client/main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"golang.org/x/crypto/ssh"
 	"golang.org/x/net/websocket"
-	"io"
+	"log"
 	"os"
-	"sync"
 )
 
 func main() {
@@ -18,25 +19,47 @@ func main() {
 		panic(err)
 	}
 
-	wg := sync.WaitGroup{}
-	wg.Add(2)
+	sshClient, err := wrapSSH(wsConn, url)
 
-	go func() {
-		_, err := io.Copy(wsConn, os.Stdin)
-		if err != nil {
-			fmt.Println(err.Error())
-		}
-		wg.Done()
-	}()
+	if err != nil {
+		panic(err)
+	}
 
-	go func() {
-		_, err := io.Copy(os.Stdout, wsConn)
-		if err != nil {
-			fmt.Println(err.Error())
-		}
-		wg.Done()
-	}()
+	// Each ClientConn can support multiple interactive sessions,
+	// represented by a Session.
+	session, err := sshClient.NewSession()
+	if err != nil {
+		log.Fatal("Failed to create session: ", err)
+	}
+	defer session.Close()
 
-	wg.Wait()
-	_ = wsConn.Close()
+	// Once a Session is created, you can execute a single command on
+	// the remote side using the Run method.
+	var b bytes.Buffer
+	session.Stdout = &b
+	if err := session.Run("/usr/bin/whoami"); err != nil {
+		log.Fatal("Failed to run: " + err.Error())
+	}
+	fmt.Println(b.String())
+}
+
+func wrapSSH(conn *websocket.Conn, addr string) (*ssh.Client, error) {
+	// An SSH client is represented with a ClientConn.
+	//
+	// To authenticate with the remote server you must pass at least one
+	// implementation of AuthMethod via the Auth field in ClientConfig,
+	// and provide a HostKeyCallback.
+	config := &ssh.ClientConfig{
+		User: "testuser",
+		Auth: []ssh.AuthMethod{
+			ssh.Password("yourpassword"),
+		},
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+	}
+
+	c, chans, reqs, err := ssh.NewClientConn(conn, addr, config)
+	if err != nil {
+		return nil, err
+	}
+	return ssh.NewClient(c, chans, reqs), nil
 }
--- go.mod
+++ go.mod
@@ -3,3 +3,8 @@ module github.com/domdom82/ws-proxy
 go 1.22.1
 
 require golang.org/x/net v0.22.0
+
+require (
+	golang.org/x/crypto v0.22.0 // indirect
+	golang.org/x/sys v0.19.0 // indirect
+)
